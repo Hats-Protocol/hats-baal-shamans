6821e91af09cd77b22cced7302794f9e6c732891
spengrah <code@spengrah.xyz>
2023-09-11 17:29:59 -0500

    prep for integration tests

---
+++ test/integration/OnboardingWithRoleStaking.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import { Test, console2 } from "forge-std/Test.sol";
+import { HatsOnboardingShaman } from "../../src/HatsOnboardingShaman.sol";
+import { HatsRoleStakingShaman } from "../../src/HatsRoleStakingShaman.sol";
+
+contract IntegrationTest is Test {
+// TODO
+}
